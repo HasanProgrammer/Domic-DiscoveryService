d2ccc7d6100a41eb5c13623c1bf40503dd8b6798
Hasan Karami Moheb <hasankarami2020313@gmail.com>
2024-06-06 19:32:39 +0330

    changes

---
+++ src/Infrastructure/Domic.Infrastructure/Implementations.Domain/Repositories/Q/ConsumerEventQueryRepository.cs
@@ -0,0 +1,59 @@
+﻿using Domic.Core.Common.ClassExtensions;
+using Domic.Core.Domain.Contracts.Interfaces;
+using Domic.Core.Domain.Entities;
+using Domic.Persistence.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
+namespace Domic.Infrastructure.Implementations.Domain.Repositories.Q;
+
+public class ConsumerEventQueryRepository : IConsumerEventQueryRepository
+{
+    private readonly DBContext _dbContext;
+    
+    public ConsumerEventQueryRepository(IConfiguration configuration, IHostEnvironment hostEnvironment)
+    {
+        var connection = configuration.GetMongoConnectionString();
+
+        _dbContext = new DBContext("ConsumerEventQuery", MongoClientSettings.FromConnectionString(connection));
+    }
+
+    public ConsumerEventQuery FindById(object id)
+        => _dbContext.Find<ConsumerEventQueryModel, ConsumerEventQuery>()
+                     .Match(s => s.ID.Equals(id.ToString()))
+                     .Project(s => new ConsumerEventQuery {
+                         Id = s.ID,
+                         Type = s.Type,
+                         CountOfRetry = s.CountOfRetry,
+                         CreatedAt_EnglishDate = s.CreatedAt_EnglishDate,
+                         CreatedAt_PersianDate = s.CreatedAt_PersianDate
+                     })
+                     .ExecuteFirstAsync().Result;
+    
+    public Task<ConsumerEventQuery> FindByIdAsync(object id, CancellationToken cancellationToken) 
+        => _dbContext.Find<ConsumerEventQueryModel, ConsumerEventQuery>()
+                     .Match(s => s.ID.Equals(id.ToString()))
+                     .Project(s => new ConsumerEventQuery {
+                         Id = s.ID,
+                         Type = s.Type,
+                         CountOfRetry = s.CountOfRetry,
+                         CreatedAt_EnglishDate = s.CreatedAt_EnglishDate,
+                         CreatedAt_PersianDate = s.CreatedAt_PersianDate
+                     })
+                     .ExecuteFirstAsync(cancellationToken);
+
+    public void Add(ConsumerEventQuery entity)
+    {
+        var newModel = new ConsumerEventQueryModel {
+            ID = entity.Id,
+            Type = entity.Type,
+            CountOfRetry = entity.CountOfRetry,
+            CreatedAt_EnglishDate = entity.CreatedAt_EnglishDate,
+            CreatedAt_PersianDate = entity.CreatedAt_PersianDate
+        };
+        
+        _dbContext.SaveAsync(newModel).GetAwaiter().GetResult();
+    }
+}
---
+++ src/Infrastructure/Domic.Persistence/Models/ConsumerEventQueryModel.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Entities;
+
+namespace Domic.Persistence.Models;
+
+public class ConsumerEventQueryModel : Entity
+{
+    public string Type { get; set; }
+    public int CountOfRetry { get; set; }
+    public DateTime CreatedAt_EnglishDate { get; set; }
+    public string CreatedAt_PersianDate { get; set; }
+}
